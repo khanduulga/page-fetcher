e9872953e521f26d51f74295bc8360bfa0023308
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-12-04 00:37:53 +0000

    went over code with Lola, changed statusCode acceptance from 200-300 and tried to add catch for throw err but did not work

--- fetcher.js
+++ fetcher.js
@@ -5,13 +5,14 @@ const urlAndPath = process.argv.slice(2);
 
 request(urlAndPath[0], (error, response, body) => {
   if (error) {
-    console.log('error:', error); // Print the error if one occurred
+    console.error(error); // Print the error if one occurred
   }
-  if(response.statusCode !== 200) {
+  //allow for 200-300
+  if(response.statusCode < 200 || response.statusCode >= 300) {
     throw new Error('URL resulted in an error')
   }
   //take body and write to index.html
-  const written = fs.writeFile(urlAndPath[1], body, (err) => {
+  fs.writeFile(urlAndPath[1], body, (err) => {
     if (err) throw err;
     console.log(`Downloaded and saved ${Buffer.byteLength(body, 'utf8')} bytes to ${urlAndPath[1]}`)
   })
